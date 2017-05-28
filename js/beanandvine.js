85642bc0700c6d573bcec2aa7afbb528ac19ca07
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-05-28 08:31:34 -0700

    Update footer stuff.
    Signed-off-by: Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>

---
+++ js/beanandvine.js
@@ -0,0 +1,8 @@
+/* Set the copyright year automatically. */
+/*jslint browser:true, white:true*/
+(function () {
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("footerYear").textContent = new Date().getFullYear();
+  });
+}());
